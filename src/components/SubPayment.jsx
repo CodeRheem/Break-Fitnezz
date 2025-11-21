aeace149a3ea9d9a7ec35d641b476d883d5291fc
Ibraheem AbdulRahman <ibraheemabdulrahman305@gmail.com>
2025-11-20 22:07:05 -0800

    Quick Checkin

--- src/components/SubPayment.jsx
+++ src/components/SubPayment.jsx
@@ -25,18 +25,24 @@ export default function () {
                         Unlimited gym access.
                     </li>
                     <li className="flex items-center gap-2 mb-1">
-                        <CheckCircleIcon className="w-8 h-5 text-green-500" />
+                        <CheckCircleIcon className="w-8 h-5 text-green-500 mb-5" />
                         Participation in group fitness classes.
                     </li>
                     <li className="flex items-center gap-2 mb-1">
-                        <CheckCircleIcon className="w-8 h-5 text-green-500" />
+                        <CheckCircleIcon className="w-8 h-5 text-green-500 mb-5" />
                         Access to locker rooms and showers.
                     </li>
                 </ul>
                 <button className=" hover:bg-white hover:text-black border text-white border-white rounded-2xl p-2 text-center w-full mt-3">SUBSCRIBE NOW</button>
             </div>
-            <div className="border rounded-2xl p-16 w-80 h-85 -mt-3 bg-green-600"></div>
-            <div className="border rounded-2xl p-16 w-80 h-80"></div>
+            <div className="border rounded-2xl p-3 w-80 h-85 -mt-3 bg-green-600">
+                <h2 className="text-2xl font-extrabold">VIP</h2>
+                <h3 className="text-2xl font-extrabold mb-4">$100 <span className="font-light text-2">/month</span></h3>
+                <hr />
+            </div>
+            <div  className="border rounded-2xl p-3 w-80 h-85">
+
+            </div>
           </div>
         </div>
       </section>
