9f0c20de87750dc219f3e184ddccf46c1689a9a6
Ibraheem AbdulRahman <ibraheemabdulrahman305@gmail.com>
2025-11-22 16:42:43 -0800

    Added font-family and finished SubPayment

--- src/components/AboutFitness.jsx
+++ src/components/AboutFitness.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 
-
-export default function AboutFitness(){
-    return(
-        <div className="bg-black h-[70vh]">
-            <div className="w-full flex flex-row items-center text-white justify-between gap-16 md:flex-row  px-12">
-                <h2 className="text-5xl w-full md:w-2/5">About Break Fitness</h2>
-                <p className="w-full md:w-3/5">At Break Fitness, we are more than a gym-we are a community, with state of the art equipment,expert trainers, and a welcoming atmosphere, we help you break barriers and achieve greatness. Whether you're a beginner or seasoned athlete, we offer personalized support to ensure every workout brings you closer ro your goals.</p>
+export default function AboutFitness() {
+    return (
+        <div className="font-poppins bg-black h-[70vh] text-white">
+            <div className="w-full flex flex-row items-center justify-between gap-16 md:flex-row px-12">
+                <h2 className="text-5xl w-full md:w-2/5 font-extrabold">About Break Fitness</h2>
+                <p className="w-full md:w-3/5 font-medium">
+                    At Break Fitness, we are more than a gym—we are a community. With state-of-the-art equipment, expert trainers, and a welcoming atmosphere, we help you break barriers and achieve greatness. Whether you're a beginner or seasoned athlete, we offer personalized support to ensure every workout brings you closer to your goals.
+                </p>
             </div>
             <div className="flex flex-row gap-6 tb:flex-row mt-12 px-12">
-                <div className="flex flex-col gap-1 g items-center justify-center w-full md:w-1/4">
+                <div className="flex flex-col gap-1 items-center justify-center w-full md:w-1/4">
                     <h3 className="text-7xl tb:text-3xl lg:text-7xl text-accent font-bold text-green-600">10+</h3>
-                    <p className="text-sm text-center text-white">Years of Excellence</p>
+                    <p className="text-sm text-center">Years of Excellence</p>
                 </div>
-                <div className="flex flex-col gap-1 g items-center justify-center w-full md:w-1/4">
+                <div className="flex flex-col gap-1 items-center justify-center w-full md:w-1/4">
                     <h3 className="text-7xl tb:text-3xl lg:text-7xl text-accent font-bold text-green-600">5000+</h3>
-                    <p className="text-sm text-center text-white">Members Served</p>
+                    <p className="text-sm text-center">Members Served</p>
                 </div>
-                <div className="flex flex-col gap-1 g items-center justify-center w-full md:w-1/4">
+                <div className="flex flex-col gap-1 items-center justify-center w-full md:w-1/4">
                     <h3 className="text-7xl tb:text-3xl lg:text-7xl text-accent font-bold text-green-600">50+</h3>
-                    <p className="text-sm text-center text-white">Classes Weekly</p>
+                    <p className="text-sm text-center">Classes Weekly</p>
                 </div>
-                <div className="flex flex-col gap-1 g items-center justify-center w-full md:w-1/4">
+                <div className="flex flex-col gap-1 items-center justify-center w-full md:w-1/4">
                     <h3 className="text-7xl tb:text-3xl lg:text-7xl text-accent font-bold text-green-600">13</h3>
-                    <p className="text-sm text-center text-white">Awards Won</p>
+                    <p className="text-sm text-center">Awards Won</p>
                 </div>
             </div>
-    </div>
-    )
+        </div>
+    );
 }
--- src/components/SubPayment.jsx
+++ src/components/SubPayment.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { CheckCircleIcon } from "@heroicons/react/24/solid";
 
-export default function () {
+export default function SubPayment() {
   return (
-    <>
+    <div className="font-poppins text-white">
       <section>
-        <div className="bg-black text-white px-4 py-16">
+        <div className="bg-black px-4 py-16">
           <div className="w-full flex flex-col md:flex-row items-center justify-between gap-8 mb-12 px-10 py-10">
-            <p className="w-full md:w-2/5">
+            <p className="w-full md:w-2/5 font-medium text-lg">
               Pick a plan tailored to your fitness goals and lifestyle. Each membership offers unique benefits to help you break limits and achieve more.
             </p>
-            <h2 className="text-4xl w-full md:w-3/5">
+            <h2 className="text-4xl w-full md:w-3/5 font-extrabold">
               Perfect Plans for your Fitness Goals.
             </h2>
           </div>
           <div className="flex justify-center flex-row gap-10 px-10">
-            <div className="border rounded-2xl p-3 w-80 h-80">
+            <div className="border rounded p-3 w-90 h-80 mt-2">
                 <h2 className="text-2xl font-extrabold">Basic</h2>
                 <h3 className="text-2xl font-extrabold mb-4">$30 <span className="font-light text-2">/month</span></h3>
                 <hr />
@@ -33,19 +33,51 @@ export default function () {
                         Access to locker rooms and showers.
                     </li>
                 </ul>
-                <button className=" hover:bg-white hover:text-black border text-white border-white rounded-2xl p-2 text-center w-full mt-3">SUBSCRIBE NOW</button>
+                <button className="hover:bg-white hover:text-black border text-white border-white rounded-2xl p-2 text-center w-full mt-3 font-bold">SUBSCRIBE NOW</button>
             </div>
-            <div className="border rounded-2xl p-3 w-80 h-85 -mt-3 bg-green-600">
-                <h2 className="text-2xl font-extrabold">VIP</h2>
+            <div className="border rounded p-3 w-100 h-91 -mt-3 bg-green-600 text-black">
+                <h2 className="text-2xl font-extrabold p-2 mb-1">VIP</h2>
                 <h3 className="text-2xl font-extrabold mb-4">$100 <span className="font-light text-2">/month</span></h3>
                 <hr />
+                <ul className="p-4">
+                    <li className="flex items-center gap-2 mb-1">
+                        <CheckCircleIcon className="w-8 h-5 text-black" />
+                        All Premium Plan features
+                    </li>
+                    <li className="flex items-center gap-2 mb-1">
+                        <CheckCircleIcon className="w-8 h-5 text-black mb-5" />
+                        Unlimited personal training sessions
+                    </li>
+                    <li className="flex items-center gap-2 mb-1">
+                        <CheckCircleIcon className="w-8 h-5 text-black mb-5" />
+                        Customized nutrition coaching
+                    </li>
+                </ul>
+                <button className="hover:bg-white hover:text-black border text-black border-black rounded-2xl p-2 text-center w-full mt-10 font-bold">SUBSCRIBE NOW</button>
             </div>
-            <div  className="border rounded-2xl p-3 w-80 h-85">
-
+              <div className="border rounded p-3 w-90 h-80 mt-2">
+                    <h2 className="text-2xl font-extrabold">Premium</h2>
+                    <h3 className="text-2xl font-extrabold mb-4">$60 <span className="font-light text-2">/month</span></h3>
+                    <hr />
+                    <ul className="p-4">
+                        <li className="flex items-center gap-2 mb-1">
+                            <CheckCircleIcon className="w-8 h-5 text-green-500" />
+                            All Basic Plan features
+                        </li>
+                        <li className="flex items-center gap-2 mb-1">
+                            <CheckCircleIcon className="w-8 h-5 text-green-500 mb-5" />
+                            One personal training session per month
+                        </li>
+                        <li className="flex items-center gap-2 mb-1">
+                            <CheckCircleIcon className="w-8 h-5 text-green-500 mb-5" />
+                            Access to the sauna and steam room
+                        </li>
+                    </ul>
+                    <button className="hover:bg-white hover:text-black border text-white border-white rounded-2xl p-2 text-center w-full mt-3 font-bold">SUBSCRIBE NOW</button>
+              </div>
             </div>
-          </div>
         </div>
       </section>
-    </>
+    </div>
   );
 }
